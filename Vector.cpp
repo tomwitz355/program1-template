be3c202252d78873091d02ad1661562f46856eda
Thomas Horowitz <thorowi1@binghamton.edu>
2019-02-14 16:15:05 -0500

    Made some of the methods for vector

--- Vector.cpp
+++ Vector.cpp
@@ -1,13 +1,13 @@
 #include "Vector.h"
 
-
-Vector::Vector(){	//constructor sets up an array called vector
+//constructor sets up an array called vector
+Vector::Vector(){
 	this->vector = new Planet*[0];
 }
 
 /** for loops through vector to delete all the elements then deletes
 the vector itself
-**/
+*/
 Vector::~Vector(){
 	for(int i = 0; i < size(); i++){
 		delete vector[i];
@@ -15,10 +15,44 @@ Vector::~Vector(){
 	delete vector;
 }
 
+
+/**  I am gonna work on this more, I am not sure if this is right.
+@TODO delete temp
+*/
 void Vector::insert(int index, Planet *p){
 	if(size() < index){
+		Planet **temp = new planet*[index+1];
 		for(int i = 0; i < size(); i++){
-			
+			temp[i] = this->vector[i];
+		}
+		temp[index] = p;
+		this->vector = temp;
+		
+	}
+	else{
+		vector[index];
+	}
+}
+
+Planet* Vector::read(int index){
+	if(size() < index){
+		return NULL;
+	}
+	return this->vector[index];
+}
+
+//@TODO delete temp
+//@TODO fix return
+
+bool Vector::remove(int index){
+	Planet **temp = new planet*[size()-1]{
+	int newindex = 0;
+	for(int i = 0; i < size(); i++){
+		if(index != i){
+			temp[newindex] = vector[i];
+			newindex++;
 		}
 	}
-} 
+	vector = temp;
+	return true;
+}
